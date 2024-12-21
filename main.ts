68cbc732a06acda6419b8fa9730263c5241d2f3e
ETmodules <etmodules@outlook.com>
2024-12-21 23:16:06 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,12 @@
+//% color="#888800" icon="\u0041"
+//% block="ET EDrive"
+//% block.loc.nl="ET EDrive"
+namespace ETedrive {
+
+    //% block="ID"
+    //% block.loc.nl="ID"
+    export function id(): String {
+        return "ET-EDrive"
+    }
+
+}
